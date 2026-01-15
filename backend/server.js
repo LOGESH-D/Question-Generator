0c0400a0a3e5798d36c24dcd27a710459a2075dc
LOGESH-D <kit26.csbs30@gmail.com>
2026-01-15 10:21:12 +0530

    deployment changes

--- backend/server.js
+++ backend/server.js
@@ -2,30 +2,42 @@ import "./config/env.js";
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import authRoutes from "./routes/authRoutes.js";
 import resumeRoutes from "./routes/resumeRoutes.js";
 import questionRoutes from "./routes/questionRoutes.js";
 
-
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+
 app.use("/api/auth", authRoutes);
 app.use("/api/resume", resumeRoutes);
 app.use("/api/questions", questionRoutes);
+app.use(express.static(path.join(__dirname, "dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
-
-
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT, () =>
+      console.log(`Server running on port ${PORT}`)
+    );
   })
-  .catch(err => console.error(err));
+  .catch((err) => console.error(err));
